76578708703b58d8fda547e00dc3317a7c5909e5
famgz <famgz@proton.me>
2024-05-30 12:29:47 -0300

    feat: ensure user can choose type of media to download

--- src/ig_dl/__main__.py
+++ src/ig_dl/__main__.py
@@ -1,5 +1,5 @@
 import sys
-from famgz_utils import print
+from famgz_utils import print, input
 from pathlib import Path
 
 if __package__ is None:
@@ -12,13 +12,29 @@ if __name__ == '__main__':
     while not username:
         username = input('\nEnter a username to search or (Q) to quit\n>').strip().lower()
         if username == 'q':
+            print('\nExiting...')
             exit()
 
     from .main import scrape
     from .config import OUTPUT_DIR, session
 
+    print()
+
+    opts = {
+        'posts': False,
+        'highlights': False,
+        'stories': False,
+    }
+
+    for opt in opts:
+        opts[opt] = bool(input(f'Enter any key to get [bright_blue]{opt}[/]:\n>'))
+
+    if not any([opt for opt in opts.values()]):
+        print('\nNothing to do. Exiting...')
+        exit()
+
     try:
         print(f'[white]The files will be saved to: {OUTPUT_DIR}\\{username}')
-        scrape(username)
+        scrape(username, get_posts=opts['posts'], get_highlights=opts['highlights'], get_stories=opts['stories'])
     finally:
         session.close()

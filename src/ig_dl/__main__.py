747ddc2d916aa6a6dec520948e12cf43316bd219
famgz <famgz@proton.me>
2024-09-02 22:52:19 -0300

    chore: move output dir to .env

--- src/ig_dl/__main__.py
+++ src/ig_dl/__main__.py
@@ -5,14 +5,16 @@ from pathlib import Path
 if __package__ is None:
     sys.path.insert(0, Path(__file__).resolve().parent.parent)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    username = sys.argv[1] if len(sys.argv) > 1 else ''
+    username = sys.argv[1] if len(sys.argv) > 1 else ""
 
     while not username:
-        username = input('\nEnter a username to search or (Q) to quit\n>').strip().lower()
-        if username == 'q':
-            print('\nExiting...')
+        username = (
+            input("\nEnter a username to search or (Q) to quit\n>").strip().lower()
+        )
+        if username == "q":
+            print("\nExiting...")
             exit()
 
     from .main import scrape
@@ -21,20 +23,25 @@ if __name__ == '__main__':
     print()
 
     opts = {
-        'posts': False,
-        'highlights': False,
-        'stories': False,
+        "posts": False,
+        "highlights": False,
+        "stories": False,
     }
 
     for opt in opts:
-        opts[opt] = bool(input(f'Enter any key to get [bright_blue]{opt}[/]:\n>'))
+        opts[opt] = bool(input(f"Enter any key to get [bright_blue]{opt}[/]:\n>"))
 
     if not any([opt for opt in opts.values()]):
-        print('\nNothing to do. Exiting...')
+        print("\nNothing to do. Exiting...")
         exit()
 
     try:
-        print(f'[white]The files will be saved to: {OUTPUT_DIR}\\{username}')
-        scrape(username, get_posts=opts['posts'], get_highlights=opts['highlights'], get_stories=opts['stories'])
+        print(f"[white]The files will be saved to: {OUTPUT_DIR}\\{username}")
+        scrape(
+            username,
+            get_posts=opts["posts"],
+            get_highlights=opts["highlights"],
+            get_stories=opts["stories"],
+        )
     finally:
         session.close()
--- src/ig_dl/config.py
+++ src/ig_dl/config.py
@@ -1,18 +1,22 @@
-import requests
+from dotenv import load_dotenv
 from famgz_utils import json_, Cookies
 from pathlib import Path
+import os
+import requests
 
-_source_path = Path(__file__).resolve()
-SOURCE_DIR = _source_path.parent
-DATA_DIR = Path(SOURCE_DIR, 'data')
-COOKIES_DIR = Path(SOURCE_DIR, 'cookies')
-TEMP_DIR = Path(SOURCE_DIR, 'temp')
-OUTPUT_DIR = 'C:\\INSTAGRAM'
+SOURCE_DIR = Path(__file__).resolve().parent
+
+load_dotenv(Path(SOURCE_DIR, ".env"))
+
+DATA_DIR = Path(SOURCE_DIR, "data")
+COOKIES_DIR = Path(SOURCE_DIR, "cookies")
+TEMP_DIR = Path(SOURCE_DIR, "temp")
+OUTPUT_DIR = Path(os.environ.get("OUTPUT_DIR") or "C:/IG_DL")
 
 _root_folders = [
     path
     for name, path in dict(locals()).items()
-    if isinstance(path, Path) and name.endswith('_DIR')
+    if isinstance(path, Path) and name.endswith("_DIR")
 ]
 
 # check and create root folders
@@ -21,16 +25,16 @@ for folder in _root_folders:
         folder.mkdir(parents=True, exist_ok=True)
 
 _cookies_keys = [
-    'ig_did',
-    'mid',
-    'datr',
-    'ig_nrcb',
-    'csrftoken',
-    'ds_user_id',
-    'shbid',
-    'sessionid',
-    'rur',
-    'shbts',
+    "ig_did",
+    "mid",
+    "datr",
+    "ig_nrcb",
+    "csrftoken",
+    "ds_user_id",
+    "shbid",
+    "sessionid",
+    "rur",
+    "shbts",
 ]
 
 _cookies = Cookies(COOKIES_DIR)
@@ -40,25 +44,25 @@ _cookies.check_expired_cookies()
 cookies = _cookies.get_cookies()
 
 headers = {
-    'authority': 'www.instagram.com',
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9',
-    'referer': 'https://www.instagram.com/',
-    'sec-ch-ua': '"Brave";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-ch-ua-platform-version': '"10.0.0"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'sec-gpc': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-    'x-asbd-id': '129477',
+    "authority": "www.instagram.com",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "referer": "https://www.instagram.com/",
+    "sec-ch-ua": '"Brave";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": '""',
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-ch-ua-platform-version": '"10.0.0"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    "x-asbd-id": "129477",
     # 'x-csrftoken': '',
-    'x-ig-app-id': '936619743392459',
-    'x-ig-www-claim': '0',
-    'x-requested-with': 'XMLHttpRequest',
+    "x-ig-app-id": "936619743392459",
+    "x-ig-www-claim": "0",
+    "x-requested-with": "XMLHttpRequest",
 }
 
 session = requests.Session()
